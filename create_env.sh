415bfa718d9690af3d80a04adaa8d8e27923d932
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2025-01-27 15:14:36 +0100

    updated env creation script

--- create_env.sh
+++ create_env.sh
@@ -31,16 +31,20 @@ echo "username: $HTTP_USER"
 echo "password: $HTTP_PASS"
 echo "$(tput setaf 1)You need to remember them, storing them in a $(tput bold)password safe$(tput sgr0 setaf 1) is recommended!$(tput sgr0)"
 
+echo "NODERED_HOST=node-red" >> $FN
+echo "NODERED_PORT=1880" >> $FN
+echo "SERIAL_PORT=/dev/ttyACM0" >> $FN
+
 # gateway device
 FN=gateway/.env
 
-echo "CHIRP_TOPIC=application/00000000-0000-0000-0000-000000000001/device" > $FN
+# echo "CHIRP_TOPIC=application/00000000-0000-0000-0000-000000000001/device" > $FN
 echo "MQTT_USERNAME=lorabridge" >> $FN
 MQTT_PASS=$(tr -cd "[:alnum:]" < /dev/urandom | head -c 20)
 echo "MQTT_PASSWORD=$MQTT_PASS" >> $FN
 echo "MQTT_HOST=mqtt" >> $FN
 echo "MQTT_PORT=1883" >> $FN
-echo "MQTT_TOPIC=zigbee2mqtt" >> $FN
+# echo "MQTT_TOPIC=zigbee2mqtt" >> $FN
 
 echo "REDIS_HOST=redis" >> $FN
 echo "REDIS_PORT=6379" >> $FN
@@ -53,3 +57,10 @@ echo "DEV_STATE_TOPIC=lorabridge/state" >> $FN
 echo "CHIRPSTACK_DEV_EUI=\x${DEV_EUI^^}" >> $FN
 echo "CHIRPSTACK_DEV_KEY=\x${DEV_KEY^^}" >> $FN
 echo "CHIRPSTACK_API_SECRET=$(openssl rand -base64 32)" >> $FN
+echo "CHIRPSTACK_USER=admin" >> $FN
+echo "CHIRPSTACK_PASSWORD=admin" >> $FN
+
+echo "COUCHDB_USER=admin" >> $FN
+echo "COUCHDB_PASSWORD=couchdb" >> $FN
+echo "COUCHDB_PORT=5984" >> $FN
+echo "COUCHDB_DB=mydb" >> $FN
