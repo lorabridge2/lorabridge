322fb1a25f75bb5d54bce0c7472092b8f5befa91
tobiasdam <20456596+tobiasdam@users.noreply.github.com>
2025-01-28 14:05:12 +0100

    added script for flashing firmware and spiffs to esp

---
+++ prepare_esp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+PWD=$(pwd)
+JOIN_EUI=$(grep ^LORA_JOIN_EUI bridge/.env | cut -d "=" -f 2 | tail -n1)
+DEV_EUI=$(grep ^LORA_DEV_EUI bridge/.env | cut -d "=" -f 2 | tail -n1)
+DEV_KEY=$(grep ^LORA_DEV_KEY bridge/.env | cut -d "=" -f 2 | tail -n1)
+
+mkdir -p bridge/bridge-lorawan-tx/rpi_flashing/data
+
+FN=bridge/bridge-lorawan-tx/rpi_flashing/data/secrets.txt
+echo $DEV_KEY | tac -rs .. | echo "$(tr -d '\n')" > $FN
+echo $JOIN_EUI | tac -rs .. | echo "$(tr -d '\n')" > $FN
+echo $DEV_EUI | tac -rs .. | echo "$(tr -d '\n')" > $FN
+
+vi $FN -c "set ff=dos" -c ":wq"
+
+cd bridge/bridge-lorawan-tx/rpi_flashing
+./spiffs_config.sh
+./flash_command.sh
+
+cd $PWD
+
+rm -rf bridge/bridge-lorawan-tx/rpi_flashing/data
+rm -f bridge/bridge-lorawan-tx/rpi_flashing/spiffs.bin
